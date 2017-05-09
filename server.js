d153a08d932678f7524dcbf59e27f88bcbc0cf0b
donnie <donnie_thompson@icloud.com>
2017-05-09 14:53:37 -0700

    Fixed partials and added stylesheet

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public/stylesheets'),
+  dest: path.join(__dirname, 'public/stylesheets'),
   indentedSyntax: true,
   sourceMap: true
 }));
